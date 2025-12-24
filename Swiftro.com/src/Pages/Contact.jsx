beb6a1640d15622ebb39c765cde5eef7b8444da2
will darlington <wilfreddarlington878@gmail.com>
2025-12-24 08:55:48 +0100

    chnage the map on contact componet

--- Swiftro.com/src/Pages/Contact.jsx
+++ Swiftro.com/src/Pages/Contact.jsx
@@ -13,7 +13,7 @@ export function Contact() {
 
         
            <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8070395.890532183!2d3.37243844951931!3d9.006794485893206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0baf7da48d0d%3A0x99a8fe4168c50bc8!2sNigeria!5e0!3m2!1sen!2sng!4v1760697455109!5m2!1sen!2sng"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15864.690492588536!2d7.071504125381631!3d6.240966522997135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104382785adb3819%3A0x49315f1ca186253f!2sIfite%20Awka%20420112%2C%20Anambra!5e0!3m2!1sen!2sng!4v1760826174345!5m2!1sen!2sng"
             width="600"
             height="450"
             style={{ border: 0 }}
